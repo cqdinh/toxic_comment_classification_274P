aa58602812ebb64c72f0ad1e0b8d872020ca20d3
Christopher Dinh <cqdinh153@gmail.com>
2020-06-10 23:05:05 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,9 +3,10 @@ mkdir ~/.kaggle
 echo '{"username":"$1","key":"$2"}' > ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 ~/.local/bin/kaggle competitions download -c jigsaw-toxic-comment-classification-challenge
-apt install unzip
+apt-get install unzip
 unzip jigsaw-toxic-comment-classification-challenge.zip
 unzip train.csv.zip
 unzip test.csv.zip
 unzip test_labels.csv.zip
 pip install transformers
+apt-get install nvidia-cuda-toolkit
